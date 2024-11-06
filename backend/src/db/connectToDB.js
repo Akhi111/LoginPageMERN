286e2846d94a14e12c0411d31fd450ad70436400
Akhil <acharekarakhil@gmail.com>
2024-11-06 16:45:48 +0530

    dbconnection updated

--- backend/src/db/connectToDB.js
+++ backend/src/db/connectToDB.js
@@ -5,7 +5,7 @@ const connectToDB = async (req, res) => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to DB Successfully✅");
   } catch (error) {
-    console.log("Failed to Connect with DB❌");
+    console.log("Failed to Connect with DB❌", error.message);
   }
 };
 export default connectToDB;
